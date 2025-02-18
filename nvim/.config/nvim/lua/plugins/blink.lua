c46f7a079e8df9ed6a9b17359a7e2430d7de2dc0
Jack Rickards <jackrickards@monzo.com>
2025-02-18 23:12:43 +0000

    avante

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -5,6 +5,10 @@ return {
       keymap = {
         preset = "super-tab",
       },
+      sources = {
+        compat = { "avante_commands", "avante_mentions", "avante_files" },
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
     },
   },
 }
