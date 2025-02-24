9446c11fd8fea198c1594b7a321dc08a07ef6e33
Jack Rickards <jackrickards@monzo.com>
2025-02-24 15:00:24 +0000

    when opening file, use a picker

--- nvim/.config/nvim/lua/plugins/toolbox/init.lua
+++ nvim/.config/nvim/lua/plugins/toolbox/init.lua
@@ -56,8 +56,10 @@ local function jump_to_component(prompt_title, find_cmd, change_directory)
     confirm = function(picker, item)
       picker:close()
       local full_path = items[item.idx].full_path
-      print("Opening (default): " .. full_path)
-      vim.cmd("e " .. vim.fn.fnameescape(full_path))
+      print("Opening file picker in: " .. full_path)
+      -- Open the file picker in the selected directory
+      Snacks.picker.files({ cwd = full_path })
+      -- If change_directory is true, set the local directory after opening
       if change_directory then
         vim.cmd("lcd " .. vim.fn.fnameescape(full_path))
       end
