3458cd50268aa73b36425e0c3ab6373814701dbb
delabere <jack.rickards@hotmail.co.uk>
2025-04-18 15:32:43 +0100

    fix lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,38 +20,33 @@ local servers = {
 -- })
 --
 
-vim.lsp.config("gopls", {
-  root_dir = function(bufnr, cb)
-    local util = require("lspconfig.util")
-    local root_markers = { "main.go", "README.md", "go.mod", "LICENSE" } -- Add more as needed
-    local find_root_function = util.root_pattern(unpack(root_markers))
-    local buffer_filepath = vim.api.nvim_buf_get_name(bufnr)
-    vim.notify("buffer file path: " .. buffer_filepath)
-    if buffer_filepath == "" then
-      cb(nil)
-      return
-    end
-    local root_directory = find_root_function(buffer_filepath, bufnr)
-    vim.notify("root directory: " .. root_directory)
-    cb(root_directory)
-  end,
-
-  cmd = { "env", "GO111MODULE=off", "gopls", "-remote=auto" },
-})
-
 for _, lsp in ipairs(servers) do
   vim.lsp.enable(lsp)
 end
 
 require("lspconfig").protols.setup({})
 return {
-  -- {
-  --   dir = "~",
-  --   enabled = path:new(os.getenv("HOME") .. "/src/github.com/monzo/wearedev"):exists(),
-  --   config = function()
-  --     vim.lsp.config("gopls", {
-  --       root_dir = lspconfig.util.root_pattern("main.go", "README.md", "LICENSE")(),
-  --     })
-  --   end,
-  -- },
+  {
+    dir = "~",
+    enabled = path:new(os.getenv("HOME") .. "/src/github.com/monzo/wearedev"):exists(),
+    config = function()
+      vim.lsp.config("gopls", {
+        -- stops the lsp from trying to ingest the world
+        cmd = { "env", "GO111MODULE=off", "gopls", "-remote=auto" },
+
+        -- sets the root directory at the service level, which achieves two things:
+        -- 1. Stops lsp from ingesting the world
+        -- 2. Allows you to use <leader>sg for picker scoped to the service, and <leader>sG, for all of the monorepo
+        root_dir = function(bufnr, cb)
+          local buffer_filepath = vim.api.nvim_buf_get_name(bufnr)
+
+          local root_markers = { "main.go", "README.md", "go.mod", "LICENSE" } -- Add more as needed
+          local root_directory = lspconfig.util.root_pattern(root_markers)(buffer_filepath, bufnr)
+          cb(root_directory)
+        end,
+        --
+        filetypes = { "go", "yml", "proto" },
+      })
+    end,
+  },
 }
