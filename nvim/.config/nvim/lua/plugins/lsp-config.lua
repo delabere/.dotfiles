98520dd26a4c79ae5779a7ee80445dd14dbe78d7
delabere <jack.rickards@hotmail.co.uk>
2025-04-18 21:28:27 +0100

    small refactor

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,27 +20,27 @@ for _, lsp in ipairs(servers) do
   vim.lsp.enable(lsp)
 end
 
-local function service_root_dir(bufnr, cb)
-  local buffer_filepath = vim.api.nvim_buf_get_name(bufnr)
-  local root_markers = { "main.go", "README.md", "go.mod", "LICENSE" } -- Add more as needed
+-- service_root_dir is used to determine the root directory for the LSP server and
+-- can be used if the default root finding might cause issues, for example in a large monorepo
+local function service_root_dir(bufnum, callback)
+  local buffer_filepath = vim.api.nvim_buf_get_name(bufnum)
+  local root_markers = { "main.go", "README.md", "go.mod", "LICENSE" }
   local root_directory = lspconfig.util.root_pattern(root_markers)(buffer_filepath)
-  cb(root_directory)
+  callback(root_directory)
 end
 
-return {
-  {
-    dir = "~",
-    enabled = path:new(os.getenv("HOME") .. "/src/github.com/monzo/wearedev"):exists(),
-    config = function()
-      vim.lsp.config("gopls", {
-        cmd = { "env", "GO111MODULE=off", "gopls", "-remote=auto" },
-        root_dir = service_root_dir,
-        filetypes = { "go", "yml" },
-      })
+-- if we are in the monorepo, we want our lsps set up differently
+local monorepo_dir = path:new(os.getenv("HOME") .. "/src/github.com/monzo/wearedev")
+if monorepo_dir:exists() then
+  vim.lsp.config("gopls", {
+    cmd = { "env", "GO111MODULE=off", "gopls", "-remote=auto" },
+    root_dir = service_root_dir,
+    filetypes = { "go", "yml" },
+  })
 
-      vim.lsp.config("protols", {
-        root_dir = service_root_dir,
-      })
-    end,
-  },
-}
+  vim.lsp.config("protols", {
+    root_dir = service_root_dir,
+  })
+end
+
+return {}
