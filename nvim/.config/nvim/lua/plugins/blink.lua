1f3cb518af1fe29a1d08e9999d5bc3807195ac62
delabere <jack.rickards@hotmail.co.uk>
2025-06-23 14:13:51 +0100

    blink fix

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -3,7 +3,13 @@ return {
     "saghen/blink.cmp",
     opts = {
       keymap = {
-        preset = "super-tab",
+        ["<Tab>"] = {
+          require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        },
+        -- https://github.com/LazyVim/LazyVim/issues/6185
+        -- preset = "super-tab",
       },
       -- sources = {
       -- compat = { "avante_commands", "avante_mentions", "avante_files" },
