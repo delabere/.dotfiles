18b6bfbc8d0f2cdf1483ea7ec601beb70162b961
delabere <jack.rickards@hotmail.co.uk>
2025-10-24 10:10:57 +0100

    rename plugins and lock

--- nvim/.config/nvim/lua/plugins/small_plugins.lua
+++ nvim/.config/nvim/lua/plugins/small_plugins.lua
@@ -161,7 +161,7 @@ return {
   },
   "Hoffs/omnisharp-extended-lsp.nvim",
   {
-    "echasnovski/mini.surround",
+    "nvim-mini/mini.surround",
     version = "*",
     opts = {
       search_method = "cover_or_next",
